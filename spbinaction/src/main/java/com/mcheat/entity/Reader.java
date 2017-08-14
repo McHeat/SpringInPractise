8f697c460eda210fee40bf99b4cf338e72cbe18d
wangjingyuan <r00t@1234>
2017-08-14 09:14:41 +0800

    修改项目构建

---
+++ spbinaction/src/main/java/com/mcheat/entity/Reader.java
@@ -0,0 +1,73 @@
+package com.mcheat.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Arrays;
+import java.util.Collection;
+
+/**
+ * Created by Administrator on 2017/8/9.
+ */
+@Entity
+public class Reader implements UserDetails {
+
+    @Id
+    private String userName;
+    private String fullName;
+    private String password;
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.asList(new SimpleGrantedAuthority("READER"));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
--- spring-in-web/src/main/java/com/cn/mcheat/gen/config/RootConfig.java
+++
@@ -1,9 +0,0 @@
-package com.cn.mcheat.gen.config;
-
-import org.springframework.context.annotation.Configuration;
-
-/**
- * Created by Administrator on 2016/11/9.
- */
-public class RootConfig {
-}
